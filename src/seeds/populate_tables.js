cd490183a2c1b6fa9f66b6f3ecaaad0a457ffffe
Tim Ninan <tim.ninan@gmail.com>
2017-11-17 14:40:28 -0600

    updated goals schema to have 6 columns

--- src/seeds/populate_tables.js
+++ src/seeds/populate_tables.js
@@ -1,6 +1,6 @@
 exports.seed = function(knex, Promise) {
   return Promise.join(
-    // knex('users').del(),
+    knex('users').del(),
     // TODO: DELETE ALL ENTRIES IN EXISITING TABLES
     knex('goals').del(),
 
@@ -12,14 +12,61 @@ exports.seed = function(knex, Promise) {
     knex('users').insert({username: 'fagin', password: 'meltedbutter'}),
     knex('users').insert({username: 'felicia', password: 'sourdough'}),
     // TODO: INSERT DATA INTO TABLES
-    knex('goals').insert({user_id: 2, description: 'run in a 5k', completed: 'F'}),
-    knex('goals').insert({user_id: 1, description: 'clean room', completed: 'T'}),
-    knex('goals').insert({user_id: 1, description: 'send thank you letter', completed:'F'}),
-    knex('goals').insert({user_id: 5, description: 'meditate daily', completed:'F'}),
-    knex('goals').insert({user_id: 6, description: 'get portrait done', completed:'F'}),
-    knex('goals').insert({user_id: 5, description: 'paint something', completed:'F'}),
-    knex('goals').insert({user_id: 4, description: 'climb mount everest', completed:'F'}),
-    knex('goals').insert({user_id: 3, description: 'climb mount fuji', completed:'F'})
+    knex('goals').insert({
+        user_id: 2, 
+        title:'Do React Project',
+        description:'work on a full-stack app using React', 
+        due_date:'11-30-17', 
+        completed:'F'}),
+
+     knex('goals').insert({
+        user_id: 1, 
+        title:'Do Angular Project',
+        description:'work on a full-stack app using Angular', 
+        due_date:'12-01-17', 
+        completed:'F'}),
+
+      knex('goals').insert({
+        user_id: 2, 
+        title:'Do Vue Project',
+        description:'work on a full-stack app using Vue', 
+        due_date:'11-28-17', 
+        completed:'F'}),
+
+        knex('goals').insert({
+        user_id: 4, 
+        title:'Do Ruby Project',
+        description:'work on a full-stack app using Ruby', 
+        due_date:'12-10-17', 
+        completed:'F'}),
+
+        knex('goals').insert({
+        user_id: 1, 
+        title:'Do Python project',
+        description:'work on a full-stack app using python', 
+        due_date:'12-19-17', 
+        completed:'F'}),
+
+        knex('goals').insert({
+        user_id: 1, 
+        title:'Do MongoDB project',
+        description:'work on a full-stack app using MongoDB', 
+        due_date:'12-20-17', 
+        completed:'F'}),
+
+        knex('goals').insert({
+        user_id: 4, 
+        title:'Do PHP project',
+        description:'work on a full-stack app using PHP', 
+        due_date:'12-21-17', 
+        completed:'F'}),
+
+        knex('goals').insert({
+        user_id: 5, 
+        title:'Do C++ project',
+        description:'work on a full-stack app using C++', 
+        due_date:'12-22-17', 
+        completed:'F'})
 
   );
 };
