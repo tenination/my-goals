c457b41d98d46655cbfa3480cc234acc59c55ca1
Tim Ninan <tim.ninan@gmail.com>
2018-01-18 14:08:46 -0600

    changed variables from development to production

--- models/db.js
+++ models/db.js
@@ -1,35 +1,23 @@
 var config = require('../knexfile.js');  
-var env = 'development';  
+var env = 'production';  
 var knex = require('knex')(config[env]);
 
-// knex.schema.createTable('goals', function (table) {
-//   table.increments();
-//     table.integer('user_id').unsigned();
-//     table.foreign('user_id').references('users.user_id');
-//     table.string('title');
-//     table.string('description');
-//     table.string('due_date');
-//     table.string('completed');
-// }).then(function () {
-//   console.log('goals table is Created!');
-// });
+Promise.all([knex.schema.createTableIfNotExists('users', function (table) {
+    // TODO: DESCRIBE THE USER TABLE
+    table.increments();
+    table.string('username');
+    table.string('password');
 
-// Promise.all([knex.schema.createTableIfNotExists('users', function (table) {
-//     // TODO: DESCRIBE THE USER TABLE
-//     table.increments();
-//     table.string('username');
-//     table.string('password');
-
-//   }),
-//   knex.schema.createTableIfNotExists('goals', function (table) {
-//     table.increments();
-//     table.integer('user_id').unsigned();
-//     table.foreign('user_id').references('users.user_id');
-//     table.string('title');
-//     table.string('description');
-//     table.string('due_date');
-//     table.string('completed');
-//   })]);
+  }),
+  knex.schema.createTableIfNotExists('goals', function (table) {
+    table.increments();
+    table.integer('user_id').unsigned();
+    table.foreign('user_id').references('users.user_id');
+    table.string('title');
+    table.string('description');
+    table.string('due_date');
+    table.string('completed');
+  })]);
 
 
 module.exports = knex;
