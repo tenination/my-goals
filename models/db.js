b504337aec11d1b043906ec1a6504767feda5d83
Tim Ninan <tim.ninan@gmail.com>
2018-01-17 19:26:49 -0600

    changed db file

--- models/db.js
+++ models/db.js
@@ -2,16 +2,6 @@ var config = require('../knexfile.js');
 var env = 'production';  
 var knex = require('knex')(config[env]);
 
-return knex.schema.createTableIfNotExists('goals', function (table) {
-    table.increments();
-    table.integer('user_id').unsigned();
-    table.foreign('user_id').references('users.user_id');
-    table.string('title');
-    table.string('description');
-    table.string('due_date');
-    table.string('completed');
-});
-
 knex.schema.createTable('goals', function (table) {
   table.increments();
     table.integer('user_id').unsigned();
