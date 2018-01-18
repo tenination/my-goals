a906b803c737208b77511a16e50e85173bfd8507
Tim Ninan <tim.ninan@gmail.com>
2018-01-17 18:22:45 -0600

    modified db file

--- migrations/20180117182020_take_601.js
+++ migrations/20180117182020_take_601.js
@@ -1,8 +1,6 @@
-var config = require('../knexfile.js');  
-var env = 'development';  
-var knex = require('knex')(config[env]);
 
- knex.schema.createTableIfNotExists('goals', function (table) {
+exports.up = function(knex, Promise) {
+   return knex.schema.createTableIfNotExists('goals', function (table) {
     table.increments();
     table.integer('user_id').unsigned();
     table.foreign('user_id').references('users.user_id');
@@ -10,7 +8,9 @@ var knex = require('knex')(config[env]);
     table.string('description');
     table.string('due_date');
     table.string('completed');
- });
+  });
+};
 
-
-module.exports = knex;
+exports.down = function(knex, Promise) {
+  
+};
